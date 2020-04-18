54da10d9c0171187ff4f3eba0edbe63d903ddb6e
Renato <renatoelysiqueira@gmail.com>
2020-04-18 00:56:02 -0300

    100%

---
+++ 04. Módulo 04/photo-gallery/gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    plugins: [
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                path: __dirname + '/src/images',
+                name: 'images'
+            }
+        },
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp'
+    ]
+}
---
+++ 04. Módulo 04/photo-gallery/src/pages/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import Image from 'gatsby-image'
+
+const Index = () => {
+    const images = useStaticQuery(graphql`
+    query MyQuery {
+        allFile {
+          edges {
+            node {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }      
+    `)
+
+    return (
+        <>
+            <h1>Galeria de Imagens</h1>
+            {
+                images.allFile.edges.map(image => {
+                    return <Image fluid={image.node.childImageSharp.fluid} />
+                })
+            }
+        </>
+    )
+}
+
+export default Index
