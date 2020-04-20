bd001357db6a5153ad7b97c2d64c82b354407591
Renato <renatoelysiqueira@gmail.com>
2020-04-20 00:42:49 -0300

    100%

--- 04. Módulo 04/photo-gallery/src/pages/index.js
+++ 04. Módulo 04/photo-gallery/src/pages/index.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import Image from 'gatsby-image'
 
+import '../assets/styles.css'
+
 const Index = () => {
-    const images = useStaticQuery(graphql`
+  const images = useStaticQuery(graphql`
     query MyQuery {
         allFile {
           edges {
@@ -19,16 +21,16 @@ const Index = () => {
       }      
     `)
 
-    return (
-        <>
-            <h1>Galeria de Imagens</h1>
-            {
-                images.allFile.edges.map(image => {
-                    return <Image fluid={image.node.childImageSharp.fluid} />
-                })
-            }
-        </>
-    )
+  return (
+    <>
+      <h1>Galeria de Imagens</h1>
+      {
+        images.allFile.edges.map(image => {
+          return <Image fluid={image.node.childImageSharp.fluid} />
+        })
+      }
+    </>
+  )
 }
 
 export default Index
